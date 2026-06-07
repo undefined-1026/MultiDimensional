f3786267f1472023a4318789e7ae0a12e36591bb
undefined-1026 <aleph0-1026@qq.com>
2026-06-07 16:46:15 +0800

    修复手机着色器爆炸5

--- src/mDimension/core/MDShaders.java
+++ src/mDimension/core/MDShaders.java
@@ -8,12 +8,10 @@ import mindustry.graphics.Shaders;
 import static mindustry.graphics.Shaders.getShaderFi;
 
 public class MDShaders {
-    public static Shader wave;
     public static Shader well;
 
 
     public static void init(){
-        wave = new Shaders.SurfaceShader("crystallization-oil");
         well = new Shaders.SurfaceShader("well");
     }
 }
